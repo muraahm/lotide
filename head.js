d02de8f383234ffa8177fbc363ba01823c1903db
muraahm <ahmedmuraahm121@gmail.com>
2019-07-23 02:51:27 +0000

    head file

---
+++ head.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  const Pass = `Assertion Passed: ${actual} === ${expected}`;
+  const Fail = `Assertion Failed: ${actual} !== ${expected}`;
+  actual === expected ? console.log(Pass) : console.log(Fail);
+};
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("test", "test");
+assertEqual(1, 2);
+
+function head(array) {
+  return (array && array.length) ? array[0] : undefined;
+};
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
