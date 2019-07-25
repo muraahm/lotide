f258cb7261eddd8c948f1b88748ea0809e543b6c
muraahm <ahmedmuraahm121@gmail.com>
2019-07-25 20:19:08 +0000

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//write the code here
+
+const findKeyByValue = function(obj, value) {
+  for (let key of Object.keys(obj)) {
+    if (value === obj[key]) {
+      return key;
+    }
+  }
+
+};
+
+
+// test code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
