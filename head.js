c71c7ceca1c26a8a71c07a1cbeb0cfd38c68d8e6
muraahm <ahmedmuraahm121@gmail.com>
2019-07-29 15:50:58 +0000

    updated head with exports

--- head.js
+++ head.js
@@ -1,23 +1,15 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
- };
 
- function head(array) {
+const assertEqual = require('./assertEqual');
+let head = function(array) {
   let head;
   if (array && array.length) {
-    head = array[0] ;
-  }
-  else {
+    head = array[0];
+  } else {
     head = undefined;
   }
   return head;
-}
+};
 
 // TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
