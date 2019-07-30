c612ba23e1f5f22e342cf47a7b9dbc464430e503
muraahm <ahmedmuraahm121@gmail.com>
2019-07-30 02:01:12 +0000

    for mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,10 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+    it("returns [] for ['5']", () => {
+      assert.deepEqual(tail(['5']), []); 
+  });
+});
