d88b737f35a05d89475c02472d8dca5ecd712606
muraahm <ahmedmuraahm121@gmail.com>
2019-07-30 01:44:27 +0000

    updated fot mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,26 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
+  it("returns [1} ==> []", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [2, 2, 3] ==> [2]", () => {
+    assert.deepEqual(middle([2, 2, 3]), 2);
+  });
+  it("returns [1, 2, 3, 4, 5, 6] == [3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
+});
+
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
 
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
