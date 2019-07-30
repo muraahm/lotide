88546bcb4f48fc47fb9c1c5ae6f0de7d0d6ff8da
muraahm <ahmedmuraahm121@gmail.com>
2019-07-30 01:10:04 +0000

    updated for mocha

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,19 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+describe("#eqArrays", () => {
+  it("returns [4, 2, 3] == [4, 2, 3]", () => {
+    assert.deepEqual([4, 2, 3], [4, 2, 3])
+  });
+  it("returns [2, 2, 3] == [2, 2, 3]", () => {
+    assert.deepEqual([2, 2, 3], [2, 2, 3])
+  });
+  it("returns [1, 2, 3] == [1, 2, 3]", () => {
+    assert.deepEqual([1, 2, 4], [1, 2, 4])
+  });
+});
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+//assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
