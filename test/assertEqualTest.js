73d7d1aef4b5cca644e5b68e287e0b3ce2194012
muraahm <ahmedmuraahm121@gmail.com>
2019-07-30 01:24:18 +0000

    updated for mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,17 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("test", "test");
-assertEqual(1, 2);
+
+describe("#assertEqual", () => {
+  it("returns 1 == 1", () => {
+    assert.deepEqual([1], [1])
+  });
+  it("returns test == test", () => {
+    assert.deepEqual(['test'], ['test'])
+  });
+});
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("test", "test");
+// assertEqual(1, 2);
