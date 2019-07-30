87ccf4ee7ab16a0625526592c97585afca49d04c
muraahm <ahmedmuraahm121@gmail.com>
2019-07-30 00:22:38 +0000

    updated for mocha

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -5,6 +5,7 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 
 
+
 assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
 assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
