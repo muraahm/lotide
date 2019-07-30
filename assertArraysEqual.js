d7b329ec1b41220cab90ef8649f4e795e8c8b301
muraahm <ahmedmuraahm121@gmail.com>
2019-07-30 00:47:44 +0000

    updated for mocha

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,12 @@
 const eqArrays = require('./eqArrays');
  
-let assertArraysEqual = function(actual, expected) {
+let assertArraysEqual = function(actual, expected) { /*
   const result = eqArrays(actual, expected);
   if (result === true) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+  }*/
 };
 
 module.exports = assertArraysEqual;
